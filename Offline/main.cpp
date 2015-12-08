29c80aa2787523cc0b2f58ee73f3f6a168c81fca
Karamaz0v1 <florent.guiotte@gmail.com>
2015-12-08 15:39:13 +0100

    Start C++ project

---
+++ Offline/main.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *
+ * TODO: Project Title
+ *
+ * Author: Florent Guiotte <guiotte.florent@gmail.com>
+ *
+ **************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+
+    cout << "Offline search" << endl;
+    return 0;
+}
+
+
